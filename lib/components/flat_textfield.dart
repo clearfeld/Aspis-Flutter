e5a028bd7105669c93328a644656a95bf66f9085
Aaron Patterson <aaronpatters@gmail.com>
2023-02-14 18:28:10 -0500

    Adding full Add Manual Entry page (#1)
    Co-authored-by: Marko <49600278+clearfeld@users.noreply.github.com>

--- lib/components/flat_textfield.dart
+++ lib/components/flat_textfield.dart
@@ -6,12 +6,16 @@ class FlatTextField extends StatefulWidget {
     super.key,
     required this.textController,
     this.hintText,
+    this.labelText,
     this.password = false,
+    this.enabled = true,
   });
 
   final TextEditingController textController;
   final String? hintText;
+  final String? labelText;
   final bool password;
+  final bool enabled;
 
   @override
   State<FlatTextField> createState() => _FlatTextField();
@@ -21,9 +25,14 @@ class _FlatTextField extends State<FlatTextField> {
   @override
   Widget build(BuildContext context) {
     // final customColors = Theme.of(context).extension<CustomColors>()!;
+    Color textColor = Colors.white;
+    if (!widget.enabled) {
+      textColor = Colors.grey;
+    }
 
     return TextField(
       controller: widget.textController,
+      enabled: widget.enabled,
       obscureText: widget.password,
       enableSuggestions: !widget.password,
       autocorrect: !widget.password,
@@ -31,6 +40,8 @@ class _FlatTextField extends State<FlatTextField> {
       decoration: InputDecoration(
         filled: true,
         fillColor: const Color.fromRGBO(41, 41, 41, 1.0), // customColors.background,
+        labelText: widget.labelText,
+        labelStyle: TextStyle(color: Colors.grey, fontSize: 12),
         border: InputBorder.none,
         hintText: widget.hintText,
         hintStyle: const TextStyle(fontSize: 16.0, color: Colors.grey),
--- lib/page_manual_entry/page_manual_entry.dart
+++ lib/page_manual_entry/page_manual_entry.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:aspis/components/flat_textfield.dart';
+
+import '../components/flat_dropdown.dart';
+import 'package:expandable/expandable.dart';
 // import 'package:aspis/components/flat_dropdown.dart';
 
 class PageManualEntry extends StatefulWidget {
@@ -18,13 +21,28 @@ class _PageManualEntryState extends State<PageManualEntry> {
 
   final notesTextController = TextEditingController();
 
+  final periodTextController = TextEditingController();
+
+  final digitsTextController = TextEditingController();
+
+  final usageTextController = TextEditingController();
+
+  String groupValue = "";
+  String typeValue = "";
+  String hashValue = "";
+
   @override
   void initState() {
     super.initState();
     titleTextController.text = "";
     secretTextController.text = "";
     issuerTextController.text = "";
-    notesTextController.text = "";
+    periodTextController.text = "30";
+    digitsTextController.text = "6";
+    usageTextController.text = "0";
+    groupValue = "No Group";
+    typeValue = "TOTP";
+    hashValue = "SHA1";
   }
 
   @override
@@ -33,7 +51,38 @@ class _PageManualEntryState extends State<PageManualEntry> {
     secretTextController.dispose();
     issuerTextController.dispose();
     notesTextController.dispose();
+    periodTextController.dispose();
+    digitsTextController.dispose();
+    usageTextController.dispose();
     super.dispose();
+  }
+
+  void _moreOptionSelected(int item) {
+    if (item == 0) {
+      //Navigator.push(
+      //  context,
+      //  PageRouteBuilder(
+      //    pageBuilder:
+      //        (BuildContext context, Animation<double> animation1, Animation<double> animation2) {
+      //      return const PageAbout();
+      //    },
+      //    transitionDuration: Duration.zero,
+      //    reverseTransitionDuration: Duration.zero,
+      //  ),
+      //);
+    } else if (item == 1) {
+      print("1" * 100);
+    }
+  }
+  void _save_entry() {
+    print(titleTextController.text);
+    print(secretTextController.text);
+    print(issuerTextController.text);
+    print(notesTextController.text);
+    print(periodTextController.text);
+    print(digitsTextController.text);
+    print(usageTextController.text);
+    print(groupValue);
   }
 
   @override
@@ -43,78 +92,234 @@ class _PageManualEntryState extends State<PageManualEntry> {
         title: const Text("Add Entry"),
         elevation: 0,
         backgroundColor: const Color(0xFF006699),
+        actions: [
+          TextButton(onPressed: () => {_save_entry()}, child: const Text("Save")),
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            itemBuilder: (context) => [
+              const PopupMenuItem<int>(
+                value: 0,
+                child: Text(
+                  "Edit Icon",
+                ),
+              ),
+              const PopupMenuItem<int>(
+                value: 1,
+                child: Text(
+                  "Reset Usage Count",
+                ),
+              ),
+            ],
+            onSelected: (item) => {_moreOptionSelected(item)},
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Center(
             child: SizedBox(
-              width: 300,
+              width: 500,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Row(
                     children: <Widget>[
-                      const Text("Title"),
+                      Icon(
+                        Icons.person,
+                        color: Colors.black,
+                      ),
                       Expanded(
                         child: FlatTextField(
                           textController: titleTextController,
-                          hintText: "Title",
+                          labelText: "Title",
                         ),
                       ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 5,
+                  ),
                   Row(
                     children: <Widget>[
-                      const Text("Secret"),
+                      Icon(
+                        Icons.key,
+                        color: Colors.black,
+                      ),
                       Expanded(
                         child: FlatTextField(
                           textController: secretTextController,
-                          hintText: "Secret",
+                          labelText: "Secret",
                         ),
                       ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 5,
+                  ),
                   Row(
-                    children: <Widget>[
-                      const Text("Issuer (optional)"),
+                    children: [
+                      const SizedBox(
+                        width: 24.0,
+                      ),
                       Expanded(
-                        child: FlatTextField(
-                          textController: issuerTextController,
-                          hintText: "Issuer (optional)",
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            FlatTextField(
+                              textController: issuerTextController,
+                              labelText: "Issuer (optional)",
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16.0,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            FlatDropdown(
+                              value: groupValue,
+                              labelText: "Group",
+                              onValueChanged: (String? valueArg) {
+                                setState(() {
+                                  groupValue = valueArg!;
+                                });
+                              },
+                              items: ["No Group","fdsa","qwer"],
+                            )
+                          ],
                         ),
                       ),
                     ],
                   ),
-                  Row(
-                    children: const <Widget>[Text("Group")],
+                  const SizedBox(
+                    height: 5,
                   ),
                   Row(
                     children: <Widget>[
-                      const Text("Notes"),
+                      Icon(
+                        Icons.sort,
+                        color: Colors.black,
+                      ),
                       Expanded(
                         child: FlatTextField(
                           textController: notesTextController,
-                          hintText: "Notes (optional)",
+                          labelText: "Notes (optional)",
                         ),
                       ),
                     ],
                   ),
-                  const Text("Advance"),
-                  Row(
-                    children: const <Widget>[Text("Type")],
+                  const SizedBox(
+                    height: 5,
                   ),
-                  Row(
-                    children: const <Widget>[Text("Hash function")],
-                  ),
-                  Row(
-                    children: const <Widget>[Text("Period (seconds)")],
-                  ),
-                  Row(
-                    children: const <Widget>[Text("Digits")],
-                  ),
-                  Row(
-                    children: const <Widget>[Text("Usage count")],
+                  ExpandablePanel(
+                    header: Padding(
+                      padding: EdgeInsets.only(left: 24.0, top: 10),
+                      child: Text("Advanced",
+                              textAlign: TextAlign.left,
+                            ),
+                    ),
+                    collapsed: Column(children: []),
+                    expanded: Column(
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.info,
+                              color: Colors.black,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  FlatDropdown(
+                                    value: typeValue,
+                                    labelText: "Type",
+                                    onValueChanged: (String? valueArg) {
+                                      setState(() {
+                                        typeValue = valueArg!;
+                                      });
+                                    },
+                                    items: ["TOTP","HOTP","Stream","Yandex","MOTP"],
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16.0,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  FlatDropdown(
+                                    value: hashValue,
+                                    labelText: "Hash Function",
+                                    onValueChanged: (String? valueArg) {
+                                      setState(() {
+                                        hashValue = valueArg!;
+                                      });
+                                    },
+                                    items: ["SHA1","SHA256","SHA512"],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            const SizedBox(
+                              width: 24.0,
+                            ),
+                            Expanded(
+                              child: FlatTextField(
+                                textController: periodTextController,
+                                labelText: "Period (Seconds)",
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16.0,
+                            ),
+                            Expanded(
+                              child: FlatTextField(
+                                textController: digitsTextController,
+                                labelText: "Digits",
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 24.0,
+                            ),
+                            Expanded(
+                              child: FlatTextField(
+                                textController: usageTextController,
+                                labelText: "Usage Count",
+                                enabled: false,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                      ]
+                    ),
                   ),
                 ],
               ),
