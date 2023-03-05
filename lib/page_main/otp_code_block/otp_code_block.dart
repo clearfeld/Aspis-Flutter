03e12c61377240758a52f4e84a69332fc66fe3c0
Marko R <clearfeld@outlook.com>
2023-03-04 19:03:01 -0500

    Add: on tap otp code block copy code

--- lib/page_main/otp_code_block/otp_code_block.dart
+++ lib/page_main/otp_code_block/otp_code_block.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:aspis/store/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:otp/otp.dart' as LOTP;
+import 'package:flutter/services.dart';
 
 class OTPCodeBlock extends StatefulWidget {
   const OTPCodeBlock({super.key, required this.otpcode});
@@ -12,24 +16,74 @@ class OTPCodeBlock extends StatefulWidget {
 }
 
 class _OTPCodeBlockState extends State<OTPCodeBlock> {
+  String? OTPCode;
+
+  @override
+  void initState() {
+    super.initState();
+
+    GenerateOTPCode(widget.otpcode);
+    // OTPCodes = gRealm.all<OTP>();
+
+    // print(OTPCodes);
+  }
+
+  void GenerateOTPCode(otp) {
+    print(otp);
+    print(otp.secret);
+
+    try {
+      final code = LOTP.OTP.generateTOTPCodeString(
+          otp.secret, DateTime.now().millisecondsSinceEpoch,
+          length: 6, interval: 30, algorithm: LOTP.Algorithm.SHA1, isGoogle: true);
+      print(code);
+      OTPCode = code;
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return GestureDetector(
+      onTap: () async {
+        if (OTPCode != null) {
+          await Clipboard.setData(ClipboardData(text: OTPCode));
+        }
+      },
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Row(children: <Widget>[
-            Text(widget.otpcode.title),
-            const Text("  "),
-            Text(widget.otpcode.issuer ?? ""),
-            Container(
-              width: 128.0,
-              height: 128.0,
-              padding: const EdgeInsets.all(30.0),
-              child: SvgPicture.asset("assets/aegis_icons/icons/1_Primary/Allegro.svg",
-                  semanticsLabel: 'Acme Logo'),
-            ),
-          ]),
+          Row(
+            children: <Widget>[
+              Container(
+                width: 112.0,
+                height: 112.0,
+                padding: const EdgeInsets.all(30.0),
+                child: SvgPicture.asset("assets/aegis_icons/icons/1_Primary/Allegro.svg",
+                    semanticsLabel: 'Acme Logo'),
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(widget.otpcode.title),
+                      const Text("  "),
+                      Text(widget.otpcode.issuer ?? ""),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(
+                    OTPCode ?? "",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ],
+              ),
+              Text("Counter"),
+            ],
+          ),
         ],
       ),
     );
